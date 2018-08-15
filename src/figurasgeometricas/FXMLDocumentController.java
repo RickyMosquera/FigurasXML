0a58e08007661b6a8918bf27901c4b69ffd84829
ricky.mosquera <ricky.mosquera@BCOF-23V-T58591.uao.edu.co>
2018-08-15 17:54:10 -0500

    Falta todo

--- src/figurasgeometricas/FXMLDocumentController.java
+++ src/figurasgeometricas/FXMLDocumentController.java
@@ -13,6 +13,8 @@ import javafx.fxml.Initializable;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
 
 /**
  *
@@ -33,10 +35,46 @@ public class FXMLDocumentController implements Initializable {
        // System.out.println("You clicked me!");
        // label.setText("Hello World!");
     }
+    @FXML
+    private void Mouse(MouseEvent event) {
+        
+        double x = event.getX();
+        double y = event.getY();
+        
+        String m = Double.toString(x);
+        String n = Double.toString(y);
+        
+        Ptox.setText(m);
+        Ptoy.setText(n);
+ 
+    }
+     @FXML
+    private void crearCirculo(ActionEvent event) {
+
+        g.setStroke(Color.BLUEVIOLET);
+        g.setLineWidth(1);
+        g.strokeOval(350, 50, 80, 80);
+        
+    }
+    
+    @FXML
+    private void crearTrriangulo(ActionEvent event) {
+        
+    double x[] = {145, 25, 240};
+    double y[] = {25, 145, 145};
+    
+
+    g.fillPolygon(x, y, 3);
+    }
+    
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        g = lienzo.getGraphicsContext2D();
+        
+        double w = lienzo.getWidth();
+        double h = lienzo.getHeight();
     }    
     
 }
